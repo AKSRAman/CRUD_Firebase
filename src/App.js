4b0df4e2b48fa35a7c4c4921cb76781733426da4
Aman Kumar <amankumarsingh60820@gmail.com>
2023-02-14 00:31:41 +0530

    Notes added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import { db } from './firebase_config';
-import { collection, getDocs, addDoc, updateDoc, doc,deleteDoc } from "@firebase/firestore";
-import { async } from '@firebase/util';
+import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from "@firebase/firestore";
+import NotesList from './components/NotesList';
+import Search from './components/Search';
+import Header from './components/Header';
 
 function App() {
 
@@ -32,7 +34,7 @@ function App() {
   }
 
   const deleteUser = async (id) => {
-    const userdoc = doc(db, "first_user",id)
+    const userdoc = doc(db, "first_user", id)
     await deleteDoc(userdoc, userdoc)
   }
 
@@ -41,30 +43,109 @@ function App() {
   }, [])
 
 
+
+	const [notes, setNotes] = useState([
+		{
+			id: 1,
+			text: 'This is my first note!',
+			date: '15/04/2021',
+		},
+		{
+			id: 2,
+			text: 'This is my second note!',
+			date: '21/04/2021',
+		},
+		{
+			id: 3,
+			text: 'This is my third note!',
+			date: '28/04/2021',
+		},
+		{
+			id: 3,
+			text: 'This is my new note!',
+			date: '30/04/2021',
+		},
+	]);
+
+	const [searchText, setSearchText] = useState('');
+	const [darkMode, setDarkMode] = useState(false);
+
+	useEffect(() => {
+		const savedNotes = JSON.parse(
+			localStorage.getItem('react-notes-app-data')
+		);
+
+		if (savedNotes) {
+			setNotes(savedNotes);
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem(
+			'react-notes-app-data',
+			JSON.stringify(notes)
+		);
+	}, [notes]);
+
+	const addNote = (text) => {
+		const date = new Date();
+		const newNote = {
+			id: 1,
+			text: text,
+			date: date.toLocaleDateString(),
+		};
+		const newNotes = [...notes, newNote];
+		setNotes(newNotes);
+	};
+
+	const deleteNote = (id) => {
+		const newNotes = notes.filter((note) => note.id !== id);
+		setNotes(newNotes);
+	};
+
   return (
     <div className="App">
-      <h1>CRUD BY FIREBASE</h1>
-      <h2>CREATE</h2>
-      <input placeholder='Name' name='name' onChange={handleChange} />
-      <input placeholder='Email' name='email' onChange={handleChange} />
-      <input placeholder='Age' name='age' onChange={handleChange} />
-      <button onClick={createUser}>Create User</button>
-      <h2>UPDATE</h2>
-      {
-        user.map((elem, i) => {
-          return <div key={i}>
-            <h3>Name : {elem.name}</h3>
-            <h3>Email : {elem.email}</h3>
-            <h3>Age : {elem.age}</h3>
-            <button onClick={() => updateUser(elem)}>
-              Increase Age
-            </button>
-            <button onClick={() => deleteUser(elem.id)}>
-              Delete
-            </button>
-          </div>
-        })
-      }
+      <div>
+
+
+        <h1>CRUD BY FIREBASE</h1>
+        <h2>CREATE</h2>
+        <input placeholder='Name' name='name' onChange={handleChange} />
+        <input placeholder='Email' name='email' onChange={handleChange} />
+        <input placeholder='Age' name='age' onChange={handleChange} />
+        <button onClick={createUser}>Create User</button>
+        <h2>UPDATE</h2>
+        {
+          user.map((elem, i) => {
+            return <div key={i}>
+              <h3>Name : {elem.name}</h3>
+              <h3>Email : {elem.email}</h3>
+              <h3>Age : {elem.age}</h3>
+              <button onClick={() => updateUser(elem)}>
+                Increase Age
+              </button>
+              <button onClick={() => deleteUser(elem.id)}>
+                Delete
+              </button>
+            </div>
+          })
+        }
+      </div>
+      <div>
+      <div className={`${darkMode && 'dark-mode'}`}>
+			<div className='container'>
+				<Header handleToggleDarkMode={setDarkMode} />
+				<Search handleSearchNote={setSearchText} />
+				<NotesList
+					notes={notes.filter((note) =>
+						note.text.toLowerCase().includes(searchText)
+					)}
+					handleAddNote={addNote}
+					handleDeleteNote={deleteNote}
+				/>
+			</div>
+		</div>
+      </div>
     </div>
   );
 }
